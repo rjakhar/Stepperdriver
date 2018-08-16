42a077fd870a5a8ebf0d2a450ba507036119eca2
rjakhar <39563256+rjakhar@users.noreply.github.com>
2018-08-16 22:35:31 +0530

    Add files via upload

---
+++ steppercontrol_shiftreg.ino
@@ -0,0 +1,170 @@
+/*
+This sketch controls a 28BYJ-48 stepper motor via a shift register. 
+
+For details on usage, wiring, etc. see link below.
+http://projectsfromtech.blogspot.com/2014/03/arduino-shift-register-stepper-motor.html
+
+Last Edited: 3/1/2014 
+by Matthew https://projectsfromtech.blogspot.com
+
+Thanks to DrLuke from http://bildr.org/2011/02/74hc595/
+for Shift Register Control code
+*/
+
+int SER_Pin = 8;   //pin 14 on the 75HC595
+int RCLK_Pin = 9;  //pin 12 on the 75HC595
+int SRCLK_Pin = 10; //pin 11 on the 75HC595
+
+//How many of the shift registers - change this
+#define number_of_74hc595s 1 
+
+//do not touch
+#define numOfRegisterPins number_of_74hc595s * 8
+
+boolean registers[numOfRegisterPins];
+
+
+
+           
+
+
+//set all register pins to LOW
+void clearRegisters(){
+  for(int i = numOfRegisterPins - 1; i >=  0; i--){
+     registers[i] = LOW;
+  }
+} 
+
+
+//Set and display registers
+//Only call AFTER all values are set how you would like (slow otherwise)
+void writeRegisters(){
+
+  digitalWrite(RCLK_Pin, LOW);
+
+  for(int i = numOfRegisterPins - 1; i >=  0; i--){
+    digitalWrite(SRCLK_Pin, LOW);
+
+    int val = registers[i];
+
+    digitalWrite(SER_Pin, val);
+    digitalWrite(SRCLK_Pin, HIGH);
+
+  }
+  digitalWrite(RCLK_Pin, HIGH);
+
+}
+
+//set an individual pin HIGH or LOW
+void setRegisterPin(int index, boolean value){
+  registers[index] = value;
+}
+
+////////////////////////////////////////////////////////////////////
+//declare variables for the motor pins
+int motorPin1 = 0;	// Blue   - 28BYJ48 pin 1
+int motorPin2 = 1;	// Pink   - 28BYJ48 pin 2
+int motorPin3 = 2;	// yellow - 28BYJ48 pin 3
+int motorPin4 = 3;	// Orange - 28BYJ48 pin 4
+                        // Red    - 28BYJ48 pin 5 (VCC)
+
+int motorSpeed = 10;     //variable to set stepper speed
+
+
+//////////////////////////////////////////////////////////////////////////////
+void setup() {
+
+  Serial.begin(9600);
+  
+  pinMode(SER_Pin, OUTPUT);
+  pinMode(RCLK_Pin, OUTPUT);
+  pinMode(SRCLK_Pin, OUTPUT);
+
+
+  //reset all register pins
+  clearRegisters();
+  writeRegisters();
+  
+  
+}
+
+//////////////////////////////////////////////////////////////////////////////
+void loop(){
+clockwise();
+
+
+
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//set pins to ULN2003 high in sequence from 1 to 4
+//delay "motorSpeed" between each pin setting (to determine speed)
+
+void clockwise (){
+  
+ 
+  setRegisterPin(motorPin1, LOW);
+  setRegisterPin(motorPin2, HIGH);
+  setRegisterPin(motorPin3, HIGH);
+  setRegisterPin(motorPin4, LOW);
+  writeRegisters(); 
+  delay(motorSpeed);
+  
+  setRegisterPin(motorPin1, LOW);
+  setRegisterPin(motorPin2, LOW);
+  setRegisterPin(motorPin3, HIGH);
+  setRegisterPin(motorPin4, HIGH);
+  writeRegisters(); 
+  delay (motorSpeed);
+  
+  setRegisterPin(motorPin1, HIGH);
+  setRegisterPin(motorPin2, HIGH);
+  setRegisterPin(motorPin3, LOW);
+  setRegisterPin(motorPin4, LOW);
+  writeRegisters(); 
+  delay (motorSpeed);
+ 
+  */setRegisterPin(motorPin1, HIGH);
+  setRegisterPin(motorPin2, LOW);
+  setRegisterPin(motorPin3, LOW);
+  setRegisterPin(motorPin4, HIGH);
+  writeRegisters(); 
+  delay(motorSpeed);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//set pins to ULN2003 high in sequence from 4 to 1
+//delay "motorSpeed" between each pin setting (to determine speed)
+
+void counterclockwise(){
+  setRegisterPin(motorPin4, HIGH);
+  setRegisterPin(motorPin3, HIGH);
+  setRegisterPin(motorPin2, LOW);
+  setRegisterPin(motorPin1, LOW);
+  writeRegisters(); 
+  delay (motorSpeed);
+
+  setRegisterPin(motorPin4, LOW);
+  setRegisterPin(motorPin3, HIGH);
+  setRegisterPin(motorPin2, HIGH);
+  setRegisterPin(motorPin1, LOW);
+  writeRegisters(); 
+  delay(motorSpeed);
+  
+  
+  setRegisterPin(motorPin4, LOW);
+  setRegisterPin(motorPin3, LOW);
+  setRegisterPin(motorPin2, HIGH);
+  setRegisterPin(motorPin1, HIGH);
+  writeRegisters(); 
+  delay (motorSpeed);
+  
+  setRegisterPin(motorPin4, HIGH);
+  setRegisterPin(motorPin3, LOW);
+  setRegisterPin(motorPin2, LOW);
+  setRegisterPin(motorPin1, HIGH);
+  writeRegisters(); 
+  delay(motorSpeed);
+
+}
+

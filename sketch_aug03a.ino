24f585803ed79a60e42f262e471b7911fcebe89c
rjakhar <39563256+rjakhar@users.noreply.github.com>
2018-08-23 23:43:29 +0530

    Add files via upload

---
+++ sketch_aug03a.ino
@@ -0,0 +1,68 @@
+int red=3;
+int brown=4;
+int green=5;
+int white=6;//
+int dir=0;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(6,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+/*if(Serial.available()>0){
+  dir=Serial.read();
+  Serial.print("Assign Either 0 or 1:");
+  Serial.println(dir);
+}
+*/
+//if(dir=0)
+digitalWrite(3,HIGH);
+digitalWrite(4,LOW);
+digitalWrite(5,LOW);
+digitalWrite(6,HIGH);
+delay(10);
+digitalWrite(3,HIGH);
+digitalWrite(4,HIGH);
+digitalWrite(5,LOW);
+digitalWrite(6,LOW);
+delay(10);
+digitalWrite(3,LOW);
+digitalWrite(4,HIGH);
+digitalWrite(5,HIGH);
+digitalWrite(6,LOW);
+delay(10);
+digitalWrite(3,LOW);
+digitalWrite(4,LOW);
+digitalWrite(5,HIGH);
+digitalWrite(6,HIGH);
+delay(10);
+
+if (dir=1){
+  digitalWrite(3,HIGH);
+digitalWrite(4,LOW);
+digitalWrite(5,LOW);
+digitalWrite(6,HIGH);
+delay(10);
+digitalWrite(3,LOW);
+digitalWrite(4,LOW);
+digitalWrite(5,HIGH);
+digitalWrite(6,HIGH);
+delay(10);
+digitalWrite(3,LOW);
+digitalWrite(4,HIGH);
+digitalWrite(5,HIGH);
+digitalWrite(6,LOW);
+delay(10);
+digitalWrite(3,HIGH);
+digitalWrite(4,HIGH);
+digitalWrite(5,LOW);
+digitalWrite(6,LOW);
+delay(10);
+
+}
+}
+
